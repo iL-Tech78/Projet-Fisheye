fffb106f8428e58ca5debf894e3d06882c268582
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2025-06-03 11:56:03 +0200

    Etape 3,4 : Récupération des données du photographe depuis l'URL - Lecture de l'ID du photographe via URLSearchParams - Récupération des données correspondantes dans photographers.json avec fetch - Ajout d'un console.log pour vérifier la bonne récupération des données.

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,29 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographerById(id) { // fct qui me permet de récupérer l'id du photographe directement depuis l’URL.
+    try {
+        const response = await fetch('data/photographers.json'); // cherche les données JSON.
+        const data = await response.json(); // convertion en objet JavaScript.
+
+        // Je le photographe avec l'id correspondant grace a .find
+        const photographer = data.photographers.find((p) => p.id === parseInt(id)); // trouve le bon photographe en comparant les id.
+
+        console.log(photographer);
+
+        return photographer;
+    } catch (error) {
+        console.error('Erreur lors de la récupération du photographe:', error);
+    }
+}
+
+function getPhotographerIdFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('id');
+    // si photographer.html?id=243 : params.get('id') me retourne "243".
+}
+
+async function init() {
+    const photographerId = getPhotographerIdFromURL();
+    const photographerData = await getPhotographerById(photographerId);
+}
+
+init();
+
